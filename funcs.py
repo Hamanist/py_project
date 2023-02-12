9e0988e01de871e38c82c0dec5036db6ecab6804
Andrey <hamanist@gmail.com>
2023-02-12 23:57:15 +0300

    add funcs.py minus

--- funcs.py
+++ funcs.py
@@ -1,9 +1,19 @@
 def plus(a, b):
     '''
-    :param a:
-    :param b:
+    :param a: int
+    :param b: int
     :return: Cкладывает два числа
     '''
     return a + b
+
+
+def minus(c, d):
+    '''
+    :param c: int
+    :param d: int
+    :return: Вычитает два числа
+    '''
+    return c - d
+
 
 
